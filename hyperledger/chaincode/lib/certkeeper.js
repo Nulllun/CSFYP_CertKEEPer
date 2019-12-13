252dbc0a0393a35fe6b35e5583e00eb5f12b117b
Nulllun <michaeld23046e123@gmail.com>
2019-12-13 16:16:32 +0800

    add queryCertByString to chaincode

--- hyperledger/chaincode/lib/certkeeper.js
+++ hyperledger/chaincode/lib/certkeeper.js
@@ -5,7 +5,6 @@ const { Contract } = require('fabric-contract-api');
 class CertKeeper extends Contract {
 
     async initLedger(ctx){
-        console.info("========== START : Initialize Ledger ==========");
         // generate certificates for testing
         const certs = [
             {
@@ -38,14 +37,19 @@ class CertKeeper extends Contract {
                 title: 'CSCI4130',
                 grade: 'A',
             },
+            {
+                id: '6',
+                owner: 'Michael',
+                title: 'CSCI4130',
+                grade: 'A-',
+            },
         ];
 
         for (let i = 0; i < certs.length ; i++){
+            certs[i].docType = "CERT";
             await ctx.stub.putState('CERT' + certs[i].id, Buffer.from(JSON.stringify(certs[i])));
-            console.info('Added <--> ', certs[i]);
         }
 
-        console.info("========== END : Initialize Ledger ==========");
     }
 
     async queryCert(ctx, certID){
@@ -59,7 +63,7 @@ class CertKeeper extends Contract {
 
     async queryAllCert(ctx){
         const startKey = 'CERT1';
-        const endKey = 'CERT5';
+        const endKey = 'CERT6';
 
         const iterator = await ctx.stub.getStateByRange(startKey, endKey);
 
@@ -81,16 +85,40 @@ class CertKeeper extends Contract {
                 allResults.push({ Key, Record });
             }
             if (res.done) {
-                console.log('end of data');
                 await iterator.close();
-                console.info(allResults);
+                return JSON.stringify(allResults);
+            }
+        }
+    }
+
+    async queryCertByString(ctx, query){
+        const iterator = await ctx.stub.getQueryResult(query);
+        
+        const allResults = [];
+        while (true) {
+            const res = await iterator.next();
+
+            if (res.value && res.value.value.toString()) {
+                console.log(res.value.value.toString('utf8'));
+
+                const Key = res.value.key;
+                let Record;
+                try {
+                    Record = JSON.parse(res.value.value.toString('utf8'));
+                } catch (err) {
+                    console.log(err);
+                    Record = res.value.value.toString('utf8');
+                }
+                allResults.push({ Key, Record });
+            }
+            if (res.done) {
+                await iterator.close();
                 return JSON.stringify(allResults);
             }
         }
     }
 
     async insertCert(ctx, certID, owner, title, grade){
-        console.info("========== START : Insert Certificate ==========");
 
         const cert = {
             id: certID,
@@ -101,7 +129,6 @@ class CertKeeper extends Contract {
 
         await ctx.stub.putState(certID, Buffer.from(JSON.stringify(cert)));
     
-        console.info("========== END : Insert Certificate ==========")
     }
 
     async deleteCert(ctx, certID){
