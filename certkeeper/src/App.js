c716e84aab89a0ce89aa2bf687659c092f3c8847
Nulllun <michaeld23046e123@gmail.com>
2019-12-14 17:08:48 +0800

    add four new components (no content)

--- certkeeper/src/App.js
+++ certkeeper/src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Router, Route, Switch, Redirect } from "react-router";
+import { createBrowserHistory } from "history";
+import MyNavbar from './components/Navbar/Navbar.js';
+import CertViewer from './components/View/CertViewer.js';
+
+
+const hist = createBrowserHistory();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyNavbar/>
+      <Router history={hist}>
+        <Switch>
+          <Route path="/verify" component={MyNavbar} />
+          <Route path="/issue" component={MyNavbar} />
+          <Route path="/view" component={CertViewer} />
+          <Route path="/share" component={MyNavbar} />
+          {/* <Redirect from="/" to="/" /> */}
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ certkeeper/src/components/Navbar/Navbar.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar';
+// import Form from 'react-bootstrap/Form';
+// import FormControl from 'react-bootstrap/FormControl';
+// import Button from 'react-bootstrap/Button';
+
+
+
+export default class MyNavbar extends Component {
+    render() {
+        return(
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="/">CertKEEPer</Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="/verify">Verify</Nav.Link>
+                    <Nav.Link href="/issue">Issue</Nav.Link>
+                    <Nav.Link href="/view">View</Nav.Link>
+                    <Nav.Link href="/share">Share</Nav.Link>
+                </Nav>
+            </Navbar>
+        );
+    }
+}
+
