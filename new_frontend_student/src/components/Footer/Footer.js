52667b0a2f6ca9b227f5880554d14f86c72740aa
Daisy Mang <longting0709@yahoo.com.hk>
2020-04-06 14:18:20 +0800

    added a test json file and modified issue page

---
+++ new_frontend_student/src/components/Footer/Footer.js
@@ -0,0 +1,46 @@
+/*eslint-disable*/
+import React from "react";
+// nodejs library to set properties for components
+import PropTypes from "prop-types";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// material-ui core components
+import { List, ListItem } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+import styles from "assets/jss/material-kit-react/components/footerStyle.js";
+
+const useStyles = makeStyles(styles);
+
+export default function Footer(props) {
+  const classes = useStyles();
+  const { whiteFont } = props;
+  const footerClasses = classNames({
+    [classes.footer]: true,
+    [classes.footerWhiteFont]: whiteFont
+  });
+  const aClasses = classNames({
+    [classes.a]: true,
+    [classes.footerWhiteFont]: whiteFont
+  });
+  return (
+    <footer className={footerClasses}>
+      <div className={classes.container}>
+        <div className={classes.left}>
+          <List className={classes.list}>
+            <ListItem className={classes.inlineBlock}>
+              <p>All rights reserved.</p>
+            </ListItem>
+            <ListItem className={classes.inlineBlock}>
+              <p>By Lun Yin Fung, Michael & Mang Long Ting, Daisy</p>
+            </ListItem>
+          </List>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+Footer.propTypes = {
+  whiteFont: PropTypes.bool
+};
---
+++ new_frontend_student/src/views/ProfilePage/ProfilePage.js
@@ -0,0 +1,67 @@
+import React from "react";
+import classNames from "classnames";
+import { makeStyles } from "@material-ui/core/styles";
+
+// core components
+import Header from "components/Header/Header.js";
+import Footer from "components/Footer/Footer.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import HeaderLinks from "components/Header/HeaderLinks.js";
+import Parallax from "components/Parallax/Parallax.js";
+
+import profile from "assets/img/dummy-profile.png";
+
+import PersonalSection from "./Personal";
+
+import styles from "assets/jss/material-kit-react/views/profilePage.js";
+
+const useStyles = makeStyles(styles);
+
+export default function ProfilePage(props) {
+  const classes = useStyles();
+  const { ...rest } = props;
+  const imageClasses = classNames(
+    classes.imgRaised,
+    classes.imgRoundedCircle,
+    classes.imgFluid
+  );
+
+  return (
+    <div>
+      <Header
+        color="transparent"
+        brand="Material Kit React"
+        rightLinks={<HeaderLinks />}
+        fixed
+        changeColorOnScroll={{
+          height: 200,
+          color: "white"
+        }}
+        {...rest}
+      />
+      <Parallax small filter image={require("assets/img/bg-white.jpg")} />
+      <div className={classNames(classes.main, classes.mainRaised)}>
+        <div>
+          <div className={classes.container}>
+            <GridContainer justify="center">
+              <GridItem xs={12} sm={12} md={6}>
+                <div className={classes.profile}>
+                  <div>
+                    <img src={profile} alt="..." className={imageClasses} />
+                  </div>
+                </div>
+              </GridItem>
+            </GridContainer>
+          </div>
+        </div>
+      </div>
+      <div className={classNames(classes.main, classes.mainRaised)}>
+        <div className={classes.container}>
+          <PersonalSection />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
