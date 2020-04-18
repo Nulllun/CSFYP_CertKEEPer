d0952225a817d2733a00f61abfe678a114b5401e
Nulllun <michaeld23046e123@gmail.com>
2020-04-18 16:45:36 +0800

    add digest, sign, verify to CertKeeperCert

--- certkeeper-api/CertKeeperCert.js
+++ certkeeper-api/CertKeeperCert.js
@@ -4,76 +4,101 @@ const crypto = require('crypto');
 class CertKeeperCert {
     constructor() {
         this.content = {
-            // the ID of the certificat, which is unique
-            certID: "",
-            // the platform that issue the certificate (e.g. LMS)
-            issuePlatform: "",
-            // the institution that offer the course (e.g. CUHK)
-            institution: "",
-            // information of the course taken {ID, title, description}
-            courseID: "",
-            courseTitle: "",
-            courseDescription: "",
-            // information of the teachers. For example, {ID: 0001 , name: Prof. King}
-            teacherID: "",
-            teacherName: "",
-            // information of the recipient {ID, name}
-            recipientID: "",
-            recipientName: "",
-            grade: "",
-            // date in string representation
-            issueDate: "",
-            // information of the signer {ID, name, signature}
-            signerID: "",
-            signerName: ""
+            // // the ID of the certificat, which is unique
+            // certID: "",
+            // // the platform that issue the certificate (e.g. LMS)
+            // issuePlatform: "",
+            // // the institution that offer the course (e.g. CUHK)
+            // institution: "",
+            // // information of the course taken {ID, title}
+            // courseID: "",
+            // courseTitle: "",
+            // // information of the teachers. For example, {ID: 0001 , name: Prof. King}
+            // teacherID: "",
+            // teacherName: "",
+            // // information of the recipient {ID, name}
+            // recipientID: "",
+            // recipientName: "",
+            // grade: "",
+            // // date in string representation
+            // issueDate: "",
+            // // information of the signer {ID, name, signature}
+            // signerID: "",
+            // signerName: ""
         };
+        this.disclosed = false;
         this.digest = "";
         this.signature = "";
     }
 
-    readContent(json) {
-        this.content.certID = json.certID;
-        this.content.issuePlatform = json.issuePlatform;
-        this.content.institution = json.institution;
-        this.content.courseID = json.courseID;
-        this.content.courseTitle = json.courseTitle;
-        this.content.courseDescription = json.courseDescription;
-        this.content.teacherID = json.teacherID;
-        this.content.teacherName = json.teacherName;
-        this.content.recipientID = json.recipientID;
-        this.content.recipientName = json.recipientName;
-        this.content.grade = json.grade;
-        this.content.issueDate = json.issueDate;
-        this.content.signerID = json.signerID;
-        this.content.signerName = json.signerName;
-        this.digest = this.generateHash(this.content);
+    readContent(contentJson) {
+        // this.content.certID = contentJson.certID;
+        // this.content.issuePlatform = contentJson.issuePlatform;
+        // this.content.institution = contentJson.institution;
+        // this.content.courseID = contentJson.courseID;
+        // this.content.courseTitle = contentJson.courseTitle;
+        // this.content.teacherID = contentJson.teacherID;
+        // this.content.teacherName = contentJson.teacherName;
+        // this.content.recipientID = contentJson.recipientID;
+        // this.content.recipientName = contentJson.recipientName;
+        // this.content.grade = contentJson.grade;
+        // this.content.issueDate = contentJson.issueDate;
+        // this.content.signerID = contentJson.signerID;
+        // this.content.signerName = contentJson.signerName;
+        this.content = contentJson;
+        this.generateHash();
     }
 
     getContent() {
-        const json = {
-            certID: this.content.certID,
-            issuePlatform: this.content.issuePlatform,
-            institution: this.content.institution,
-            courseID: this.content.courseID,
-            courseTitle: this.content.courseTitle,
-            courseDescription: this.content.courseDescription,
-            teacherID: this.content.teacherID,
-            teacherName: this.content.teacherName,
-            recipientID: this.content.recipientID,
-            recipientName: this.content.recipientName,
-            grade: this.content.grade,
-            issueDate: this.content.issueDate,
-            signerID: this.content.signerID,
-            signerName: this.content.signerName
-        }
-        return json;
+        // const json = {
+        //     certID: this.content.certID,
+        //     issuePlatform: this.content.issuePlatform,
+        //     institution: this.content.institution,
+        //     courseID: this.content.courseID,
+        //     courseTitle: this.content.courseTitle,
+        //     courseDescription: this.content.courseDescription,
+        //     teacherID: this.content.teacherID,
+        //     teacherName: this.content.teacherName,
+        //     recipientID: this.content.recipientID,
+        //     recipientName: this.content.recipientName,
+        //     grade: this.content.grade,
+        //     issueDate: this.content.issueDate,
+        //     signerID: this.content.signerID,
+        //     signerName: this.content.signerName
+        // }
+        return this.content;
     }
 
-    generateHash(content){
+    generateHash(){
         const hash = crypto.createHash('sha256');
-        hash.update(JSON.stringify(content));
+        hash.update(JSON.stringify(this.content));
         let digest = hash.digest('hex');
+        this.digest = digest;
         return digest;
+    }
+
+    generateSign(privateKey){
+        const sign = crypto.createSign('sha256');
+        sign.update(this.digest);
+        sign.end();
+        const signature = sign.sign(privateKey, 'hex');
+        this.signature = signature;
+        return signature;
+    }
+
+    verifyHash(){
+        const hash = crypto.createHash('sha256');
+        hash.update(JSON.stringify(this.content));
+        let digest = hash.digest('hex');
+        return this.digest == digest;
+    }
+
+    verifySign(publicKey){
+        const verify = crypto.createVerify('sha256');
+        verify.update(this.digest);
+        verify.end();
+        const result = verify.verify(publicKey, this.signature, 'hex');
+        return result;
     }
 
 }
@@ -81,13 +106,13 @@ class CertKeeperCert {
 // the following is for testing
 
 // var a = new CertKeeperCert();
+// console.log(a);
 // a.readContent({
 //     certID: "Org1MSP-4v324f",
 //     issuePlatform: "KEEP",
 //     institution: "CUHK",
 //     courseID: "CSCI2100",
 //     courseTitle: "Data Structure",
-//     courseDescription: "Teaching ADT",
 //     teacherID: "IK12421",
 //     teacherName: "Irwin King",
 //     recipientID: "keep-11550",
@@ -97,23 +122,18 @@ class CertKeeperCert {
 //     signerID: "IK12421",
 //     signerName: "Iwrin"
 // });
-// console.log(a);
-// a.readContent({
-//     certID: "Org1MSP-4v324f",
-//     issuePlatform: "KEEP",
-//     institution: "CUHK",
-//     courseID: "CSCI2100",
-//     courseTitle: "Data Structure",
-//     courseDescription: "Teaching ADT",
-//     teacherID: "IK2421",
-//     teacherName: "Irwin King",
-//     recipientID: "keep-11550",
-//     recipientName: "Michael Lun",
-//     grade: "A+",
-//     issueDate: "Today",
-//     signerID: "IK12421",
-//     signerName: "Iwrin"
+
+// const keyPair1 = crypto.generateKeyPairSync('rsa', {
+//     modulusLength: 2048,
+//     publicKeyEncoding: {
+//       type: 'spki',
+//       format: 'pem'
+//     },
+//     privateKeyEncoding: {
+//       type: 'pkcs8',
+//       format: 'pem',
+//     }
 // });
 // console.log(a);
 
-module.exports.CertKeeperCert = CertKeeperCert;
+module.exports = CertKeeperCert;
